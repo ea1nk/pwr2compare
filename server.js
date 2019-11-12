1f8910e084f137356a79ee025a1d441feeb30a93
ea1nk <juan@ea1nk.com>
2019-11-12 17:35:53 +0100

    Add argv

--- server.js
+++ server.js
@@ -1,40 +1,56 @@
+var myArgs = process.argv.slice(2)
+if(myArgs.length < 2){
+    console.log("Error: Please, provide power meter Id's ")
+    return
+}
+
+//const PowerMeterA_Id = 847; //Tacx Neo
+var PowerMeterA_Id = myArgs[0]
+var PowerMeterB_Id = myArgs[1]
+//const PowerMeterB_Id = 22043; //Assioma Duo
+if(!myArgs[2]){
+    var average_factor = 3 //3 sec averaging default
+} else {
+    var average_factor = myArgs[2]
+}
+var start_timeA = Date.now()
+var start_timeB = Date.now()
+var measureBufferA = []
+var measureBufferB = []
+
 //ANT+ receiver
-const Ant = require('ant-plus');
-let AntPlusStick = new Ant.GarminStick2();
-let PowerMeterA = new Ant.BicyclePowerSensor(AntPlusStick);
-let PowerMeterB = new Ant.BicyclePowerSensor(AntPlusStick);
-const PowerMeterA_Id = 847; //Tacx Neo
-const PowerMeterB_Id = 22043; //Assioma Duo
-var average_factor = 3 //3 sec averaging
-var start_time = Date.now()
-var measureBuffer = []
+const Ant = require('ant-plus')
+let AntPlusStick = new Ant.GarminStick2()
+let PowerMeterA = new Ant.BicyclePowerSensor(AntPlusStick)
+let PowerMeterB = new Ant.BicyclePowerSensor(AntPlusStick)
+
 
 AntPlusStick.on('startup', function(){
-    console.log("Ant+ device ready");
+    console.log("Ant+ device ready")
     console.log("Averaging measures to " + average_factor + " seconds \n")
-    PowerMeterA.attach(0,PowerMeterA_Id);
-    PowerMeterB.attach(1,PowerMeterB_Id);
+    PowerMeterA.attach(0,PowerMeterA_Id)
+    PowerMeterB.attach(1,PowerMeterB_Id)
 
 })
 
 if(!AntPlusStick.open()){
-    console.log("Ant+ device not found!");
+    console.log("Ant+ device not found!")
 }
 
 PowerMeterA.on('powerData', AntData =>{
     
-    let time = Date.now(); //Time of measure
-    if(time - start_time > average_factor*1000){
-        AntData.timestamp = time;
-        AntData.Power = average(measureBuffer)
+    let timeA = Date.now(); //Time of measure
+    if(timeA - start_timeA > average_factor*1000){
+        AntData.timestamp = timeA;
+        AntData.Power = average(measureBufferA)
         AntData.Averaging = average_factor
         let data2send = JSON.stringify({"measures0":AntData})
-        console.log("["+ new Date(start_time) + "] " + data2send)
+        console.log("["+ new Date(start_timeA) + "] " + data2send)
         if(sockets[0]) sockets[0].send(data2send)
-        start_time = time
-        measureBuffer = []
+        start_timeA = timeA
+        measureBufferA = []
     } else {
-        measureBuffer.push(AntData.Power)
+        measureBufferA.push(AntData.Power)
     }
     
 })
@@ -45,18 +61,18 @@ PowerMeterA.on('error', err =>{
 
 PowerMeterB.on('powerData', AntData =>{
     
-    let time = Date.now(); //Time of measure
-    if(time - start_time > average_factor*1000){
-        AntData.timestamp = time;
-        AntData.Power = average(measureBuffer);
+    let timeB = Date.now(); //Time of measure
+    if(timeB - start_timeB > average_factor*1000){
+        AntData.timestamp = timeB;
+        AntData.Power = average(measureBufferB);
         AntData.Averaging = average_factor
         let data2send = JSON.stringify({"measures1":AntData})
-        console.log("["+ new Date(start_time) + "] " + data2send)
+        console.log("["+ new Date(start_timeB) + "] " + data2send)
         if(sockets[0]) sockets[0].send(data2send);
-        start_time = time
-        measureBuffer = []
+        start_timeB = timeB
+        measureBufferB = []
     } else {
-        measureBuffer.push(AntData.Power)
+        measureBufferB.push(AntData.Power)
     }
 })
 
@@ -64,41 +80,41 @@ PowerMeterB.on('error', err =>{
 
 })
 // HTTP server
-const express = require('express');
-const app = express();
-const port = 8090;
-app.use(express.static(__dirname + "/public"));
+const express = require('express')
+const app = express()
+const port = 8090
+app.use(express.static(__dirname + "/public"))
 app.use('*', function(req, res){
     //Static Server
 })
 
 app.listen(port, function(){
-    console.log("App running http://localhost:" + port);
+    console.log("App running http://localhost:" + port)
 });
 
 //WebSocket Server
 const sockets = [];
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 const ws_port = 8091;
 const wss = new WebSocket.Server({
     port:ws_port
 });
 
 wss.on('connection', function connection(ws){
-    console.log('Client connected');
-    sockets[0] = ws;
+    console.log('Client connected')
+    sockets[0] = ws
     ws.on('message', function incoming(message){
-        console.log('Received: %s', message);
+        console.log('Received: %s', message)
     })
     ws.send(JSON.stringify({"devices":[PowerMeterA_Id, PowerMeterB_Id]}))
 })
 
 function average(powerData){
-    var measures = powerData.length;
+    var measures = powerData.length
     var sum = 0;
     for(var i=0;i<measures;i++){
         sum += powerData[i]
     }
 
-    return(sum/measures);
+    return(sum/measures)
 }
